33c719fea589f66ee6774dffdcc2df519ae77842
Nick Guerra <nickg@maliciousgames.com>
2019-08-02 20:37:41 -0500

    about me page info

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
-
+import profilepic1 from "../img/profilepic1.jpg"
+// TODO: Need to edit pictures in GIMP 
 class AboutPage extends React.Component {
   render() {
     return (
@@ -12,45 +13,42 @@ class AboutPage extends React.Component {
           <h2 className="sm-heading">Here are a few things about me...</h2>
           <div className="about-info">
             <img
-              src="img/profilepic2.jpg"
+              src={profilepic1}
               alt="Nick Guerra"
               className="bio-image"
             />
             <div className="bio">
               <h3 className="text-secondary">BIO</h3>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-                hic nobis fuga tempora! Sunt, veritatis voluptate. Dolores sit,
-                quisquam aperiam necessitatibus dignissimos similique, placeat
-                voluptates praesentium eveniet autem itaque saepe!
+                I am a self taught software developer with a passion for all types of technology. I have always loved solving
+                problems, and I believe as a developer that I will never stop learning new ways to do so.
               </p>
             </div>
 
             <div className="job job-1">
-              <h3>123 Webshop</h3>
-              <h6>Full Stack Developer</h6>
+              <h3>Full Stack Developer</h3>
+              <h6>.Net and Javascript</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laboriosam, debitis. Corrupti doloribus libero eius nihil,
-                corporis optio nemo molestiae laboriosam!
+                I love to develop full stack applications, that are complete solutions. I
+                develop in both .Net and Javascript, but use any tool needed for the job at hand.
               </p>
             </div>
             <div className="job job-2">
-              <h3>Frontend ABC</h3>
-              <h6>FrontEnd</h6>
+              <h3>Game Designer</h3>
+              <h6>Unity</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laboriosam, debitis. Corrupti doloribus libero eius nihil,
-                corporis optio nemo molestiae laboriosam!
+                I am part owner and developer at Malicious Games LLC. Our team
+                works on developing games for the Unity Engine. Our first title,
+                Xenia's Ark is available for early access on the Steam Workshop.
               </p>
             </div>
             <div className="job job-3">
-              <h3>Backend DEF</h3>
-              <h6>SQL DBA</h6>
+              <h3>Web Development</h3>
+              <h6>Guerra Technology</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laboriosam, debitis. Corrupti doloribus libero eius nihil,
-                corporis optio nemo molestiae laboriosam!
+                I develop for the web in various technologies. I build websites for
+                my personal projects, and for others. I also build web Apis and
+                custom software.
               </p>
             </div>
           </div>
