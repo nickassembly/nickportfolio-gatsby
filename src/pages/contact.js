86d862ef67abf79c9ef3f928cc4164a881a78700
Nick Guerra <nickg@maliciousgames.com>
2019-08-05 20:43:12 -0500

    uncommented contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,36 +1,36 @@
-// import React from "react"
-// import Layout from "../components/layout"
+import React from "react"
+import Layout from "../components/layout"
 
-// import Head from '../components/head'
+import Head from '../components/head'
 
-// class ContactPage extends React.Component {
-//   render() {
-//     return (
-//       <Layout>
-//         <Head title="Contact" />
-//         <main id="contact">
-//           <h1 className="lg-heading">
-//             Contact <span className="text-secondary"> Me</span>
-//           </h1>
-//           <h2 className="sm-heading">How you can reach me...</h2>
+class ContactPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Head title="Contact" />
+        <main id="contact">
+          <h1 className="lg-heading">
+            Contact <span className="text-secondary"> Me</span>
+          </h1>
+          <h2 className="sm-heading">How you can reach me...</h2>
 
-//           <div className="boxes">
-//             <div>
-//               <span className="text-secondary">Email: </span>
-//               Nickg@maliciousgames.com
-//             </div>
-//             <div>
-//               <span className="text-secondary">Phone: </span>(225)290-2080
-//             </div>
-//             <div>
-//               <span className="text-secondary">Address: </span>New Orleans, LA
+          <div className="boxes">
+            <div>
+              <span className="text-secondary">Email: </span>
+              Nickg@maliciousgames.com
+            </div>
+            <div>
+              <span className="text-secondary">Phone: </span>(225)290-2080
+            </div>
+            <div>
+              <span className="text-secondary">Address: </span>New Orleans, LA
 
-//             </div>
-//           </div>
-//         </main>
-//       </Layout>
-//     )
-//   }
-// }
+            </div>
+          </div>
+        </main>
+      </Layout>
+    )
+  }
+}
 
-// export default ContactPage
+export default ContactPage
