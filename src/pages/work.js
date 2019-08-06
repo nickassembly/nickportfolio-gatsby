e14dc2b8c7010f07d70c2c6f94eea141709eae55
Nick Guerra <nickg@maliciousgames.com>
2019-08-06 05:49:45 -0500

    adjusted project page

--- src/pages/work.js
+++ src/pages/work.js
@@ -6,7 +6,7 @@ import project1Pic from "../img/projects/project1.jpg"
 import project2Pic from "../img/projects/project2.jpg"
 import project3Pic from "../img/projects/project3.jpg"
 import project4Pic from "../img/projects/project4.jpg"
-import project5Pic from "../img/projects/project5.jpg"
+
 
 import Head from '../components/head'
 import Footer from '../components/footer'
@@ -24,33 +24,10 @@ class WorkPage extends React.Component {
           <h2 className="sm-heading">Below are some of my projects...</h2>
 
           <div className="projects">
-            <div className="item">
-              <a href="https://github.com/nickassembly/ShoppingList">
-                <img src={project1Pic} alt="Project" />
-              </a>
-              <a href="https://github.com/nickassembly/ShoppingList" className="btn-light">
-                <i className="fas fa-eye"></i>Shopping List
-              </a>
-              <a href="https://github.com/nickassembly/ShoppingList" className="btn-dark">
-                <i className="fab fa-github"></i>Github
-              </a>
-            </div>
-
-            <div className="item">
-              <a href="https://github.com/nickassembly/LeitnerProject">
-                <img src={project2Pic} alt="Projects" />
-              </a>
-              <a href="https://github.com/nickassembly/LeitnerProject" className="btn-light">
-                <i className="fas fa-eye"></i>Leitner Quiz App
-              </a>
-              <a href="https://github.com/nickassembly/LeitnerProject" className="btn-dark">
-                <i className="fab fa-github"></i>Github
-              </a>
-            </div>
 
             <div className="item">
               <a href="https://github.com/nickassembly/audiobooks">
-                <img src={project3Pic} alt="Projects" />
+                <img src={project1Pic} alt="Projects" />
               </a>
               <a href="https://github.com/nickassembly/audiobooks" className="btn-light">
                 <i className="fas fa-eye"></i>Audiobook Tracker
@@ -61,20 +38,20 @@ class WorkPage extends React.Component {
             </div>
 
             <div className="item">
-              <a href="https://github.com/nickassembly/Audiobooks.API">
-                <img src={project4Pic} alt="Projects" />
+              <a href="https://github.com/nickassembly/ShoppingList">
+                <img src={project2Pic} alt="Project" />
               </a>
-              <a href="https://github.com/nickassembly/Audiobooks.API" className="btn-light">
-                <i className="fas fa-eye"></i>Audiobooks API
+              <a href="https://github.com/nickassembly/ShoppingList" className="btn-light">
+                <i className="fas fa-eye"></i>Shopping List
               </a>
-              <a href="https://github.com/nickassembly/Audiobooks.API" className="btn-dark">
+              <a href="https://github.com/nickassembly/ShoppingList" className="btn-dark">
                 <i className="fab fa-github"></i>Github
               </a>
             </div>
 
             <div className="item">
               <a href="https://github.com/nickassembly/Weather">
-                <img src={project5Pic} alt="Projects" />
+                <img src={project3Pic} alt="Projects" />
               </a>
               <a href="https://github.com/nickassembly/Weather" className="btn-light">
                 <i className="fas fa-eye"></i>Current Weather
@@ -83,6 +60,19 @@ class WorkPage extends React.Component {
                 <i className="fab fa-github"></i>Github
               </a>
             </div>
+
+            <div className="item">
+              <a href="https://github.com/nickassembly/react-website">
+                <img src={project4Pic} alt="Projects" />
+              </a>
+              <a href="https://github.com/nickassembly/react-website" className="btn-light">
+                <i className="fas fa-eye"></i>Basic React Website
+              </a>
+              <a href="https://github.com/nickassembly/react-website" className="btn-dark">
+                <i className="fab fa-github"></i>Github
+              </a>
+            </div>
+
           </div>
         </main>
         <Footer />
