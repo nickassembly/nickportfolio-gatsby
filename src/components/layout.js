ec4d77432398dadf9805467e639bd8cfc2790d4a
Nick Guerra <nickg@maliciousgames.com>
2019-09-14 06:10:06 -0500

    changed navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,13 @@
 import React from "react"
 import Navigation from "./navigation"
-import NavMenu from "./navMenu"
+
 
 
 class Layout extends React.Component {
   render() {
     return (
       <div>
-        <NavMenu />
+        <Navigation />
         <div>{this.props.children}</div>
       </div>
     )
